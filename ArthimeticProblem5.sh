82e95b73690b9909c3709018b4d7a62bd708779b
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-08-31 23:27:51 +0530

    [Nadeem] Computing Arthimetic Problem-4 for compute a % b + c

---
+++ ArthimeticProblem5.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -x
+
+
+	echo "Enter Value a"
+	read a
+	echo "Enter Value b"
+	read b
+	echo "Enter Value c"
+	read c
+
+	result1=$(( $a + $b * $c))
+	echo "result1 is=" $result1
+
+	result2=$(( $a * $b + $c))
+	echo "result2 is=" $result2
+
+	result3=$(( $c + $a / $b ))
+	echo "result3 is=" $result3
+
+	result4=$(( $a % $b + $c))
+	echo "result4 is=" $result4
